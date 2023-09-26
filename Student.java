a8afc5a9e5238905852255624dc3af8e1006a1c2
JhonatanDczel <jariasq@unsa.edu.pe>
2023-09-26 18:58:13 -0500

    Archivos y .gitignore

---
+++ Student.java
@@ -0,0 +1,20 @@
+import java.io.*;
+public class Student {
+    private BufferedReader lector;
+    private String linea;
+    private String partes [] = null;
+
+    public void leerArchivos(String nArchivo) {
+        FileReader arc = new FileReader(nArchivo);
+        lector = new BufferedReader(arc);
+        while (( linea = lector.readLine() ) != null) {
+            partes = linea.split(",");
+            imprimirLinea();
+            System.out.println();
+        }
+    }
+    private void imprimirLinea(){
+        for (int i = 0; i < partes.length; i++) System.out.print(partes[i] + " | ");
+    }
+
+}
