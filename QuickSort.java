6124df5d784a740b6f78ee3f16bec0f83cb690da
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2023-09-30 23:22:13 -0500

    Se actualizaron los archivos Student y QuickSort

--- QuickSort.java
+++ QuickSort.java
@@ -3,8 +3,14 @@ import java.io.*;
 public class QuickSort {
     public static void main(String[] args) {
         Student s = new Student();
+        Student[] array;
+        
         try {
-            impt(s.mkStulist("data.csv"));
+            array = s.mkStulist("data.csv");
+            impt(array);
+            quicksort(array, 0, array.length - 1);
+            System.out.println("----------");
+            impt(array);
         } catch (FileNotFoundException e){
             System.err.println("El archivo no se encontró: " + e.getMessage());
         } catch (IOException e) {
@@ -14,8 +20,33 @@ public class QuickSort {
     }
     public static void impt (Student [] array) {
         for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i].getName());
             System.out.println(array[i].getCui());
         }
     }
+    public static void quicksort(Student array[], int izq, int der) {
+
+  int pivote=array[izq].getCui(); // tomamos primer elemento como pivote
+  int i=izq;         // i realiza la búsqueda de izquierda a derecha
+  int j=der;         // j realiza la búsqueda de derecha a izquierda
+  int aux;
+ 
+  while(i < j){                          // mientras no se crucen las búsquedas                                   
+     while(array[i].getCui() <= pivote && i < j) i++; // busca elemento mayor que pivote
+     while(array[j].getCui() > pivote) j--;           // busca elemento menor que pivote
+     if (i < j) {                        // si no se han cruzado                      
+         aux= array[i].getCui();                      // los intercambia
+         array[i].setCui(array[j].getCui());
+         array[j].setCui(aux);
+     }
+   }
+   
+   array[izq].setCui(array[j].getCui());      // se coloca el pivote en su lugar de forma que tendremos                                    
+   array[j].setCui(pivote);      // los menores a su izquierda y los mayores a su derecha
+   
+   if(izq < j-1)
+      quicksort(array,izq,j-1);          // ordenamos subarray izquierdo
+   if(j+1 < der)
+      quicksort(array,j+1,der);          // ordenamos subarray derecho
+   
+}
 }
--- Student.java
+++ Student.java
@@ -1,78 +1,106 @@
-public class Student{
-    private String cui;
+import java.util.*;
+import java.io.*;
+public class Student {
+    // Atributos de objeto student
+    private int cui;
     private String email;
     private String name;
-    private String lastNameF;
-    private String lastNameM;
-    private String dateOfBirth;
+    private String patSurname;
+    private String matSurname;
+    private String birthDate;
     private String gender;
     private String status;
+    // Atributos de lectura de archivos
+    private BufferedReader reader;
+    private String line;
+    private String parts [] = null;
 
-    public String getCui(){
-        return cui;
+    private void imprimirLinea(){
+        for (int i = 0; i < parts.length; i++) System.out.print(parts[i] + " | ");
     }
 
-    public void setCui(String cui){
-        this.cui = cui;
+    public Student [] mkStulist (String filename) throws FileNotFoundException, IOException {
+        FileReader file = new FileReader(filename);
+        reader = new BufferedReader(file);
+        List <Student> sList = new ArrayList <Student>();
+        line = reader.readLine();
+        while ((line = reader.readLine()) != null) {
+            parts = line.split(",");
+            if (parts.length < 4) break;
+            Student s = new Student();
+            sList.add(s);
+            s.cui = Integer.valueOf( parts[0] );
+            s.email = parts[1];
+            s.name = parts[2];
+            s.patSurname = parts[3];
+            s.matSurname = parts[4];
+            s.birthDate = parts[5];
+            s.gender = parts[6];
+            s.status = parts[7];
+        }
+        Student [] array = new Student[sList.size()];
+        array = sList.toArray(array);
+        return array;
     }
 
-    public String getEmail(){
+    public int getCui () {
+        return Integer.valueOf(cui);
+    }
+    public String getEmail () {
         return email;
     }
-
-    public void setEmail(String email){
-        this.email = email;
-    }
-
-    public String getName(){
+    public String getName () {
         return name;
     }
-
-    public void setName(String name){
-        this.name = name;
+    public String getPatSurname () {
+        return patSurname;
     }
-
-    public String getLastNameF(){
-        return lastNameF;
+    public String getMatSurname () {
+        return matSurname;
     }
-
-    public void setLastNameF(String lastNameF){
-        this.lastNameF = lastNameF;
+    public String getBirthDate () {
+        return birthDate;
     }
-
-    public String getLastNameM(){
-        return lastNameM;
-    }
-
-    public void setLastNameM(String lastNameM){
-        this.lastNameM = lastNameM;
-    }
-
-    public String getDateOfBirth(){
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(String dateOfBirth){
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public String getGender(){
+    public String getGender () {
         return gender;
     }
-
-    public void setGender(String gender){
-        this.gender = gender;
-    }
-
-    public String getStatus(){
+    public String getStatus () {
         return status;
     }
-
-    public void setStatus(String status){
-        this.status = status;
+    //Métodos de establecimiento
+    public void setCui (int x) {
+        cui = x;
     }
-
-    public String toString(){
-        return "CUI: " + cui + " Email: " + email + " Nombre: " + name + " Apellido Paterno: " + lastNameF + " Apellido Materno: " + lastNameM + " Fecha de Nacimiento: " + dateOfBirth + " Genero: " + gender + " Estado: " + status;
+    public void setEmail (String x) {
+        email = x;
+    }
+    public void setName (String x) {
+        name = x;
+    }
+    public void setPatSurname (String x) {
+        patSurname = x;
+    }
+    public void setMatSurname (String x) {
+        matSurname = x;
+    }
+    public void setBirthDate (String x) {
+        birthDate = x;
+    }
+    public void setGender (String x) {
+        gender = x;
+    }
+    public void setStatus (String x) {
+        status = x;
     }
 }
+/*
+   Necesitamos un método que nos cree un nuevo objeto student a partir del data.csv,
+   Por medio del array que se obtiene, le declaramos en ese mismo método todos sus atributos
+
+   Necesitamos que los algoritmos reciban un array de objetos, así accedemos a sus atributos
+   De esa manera los ordenamos
+
+
+   PENDIENTES ---------------------------------
+   Realizar metodos de modificación y acceso al resto de atributos
+*/
