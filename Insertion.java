4fe74101fa064fa87663c6f674902add0afde893
Jorge_Luis <jmamanihuars@unsa.edu.pe>
2023-10-01 08:42:24 -0500

    Implementando los métodos que usan el algoritmo de inserción para ordenar según nombre y apellidos. Tambien se ha creado un método privado necesario para hacer las comparaciones de los Strings. Por ultimo se corrigió algunos errores de sintaxis y pequeños cambios para mayor comprensión y eficiencia

--- Insertion.java
+++ Insertion.java
@@ -18,27 +18,88 @@ public class Insertion{
     for(int i = 0; i < compañeritos.length; i++){
       lowest = i;
       pivot = compañeritos[i];
-      while((lowest > 0) && (compareEmail(compañeritos[i].getEmail(), pivot.getEmail()))){
+      while((lowest > 0) && (compareEmail(compañeritos[lowest - 1].getEmail(), pivot.getEmail()))){
         compañeritos[lowest] = compañeritos[lowest - 1];
         lowest--;
       }
       compañeritos[lowest] = pivot;
     }
   }
-  public static boolean compareEmail(String word1, String word2){
-    int indexWord1 = word1.indexOf('@');
-    int indexWord2 = word2.indexOf('@');
-    int length = word1.substring(0, indexWord1).length();
-    if(length > word2.substring(0, indexWord2).length()){
-      length = word2.substring(0, indexWord2).length();
+  public static void insertionAlgorithmName(Student[] compañeritos){
+    int lowest;
+    Student pivot;
+    for(int i = 0; i < compañeritos.length; i++){
+      lowest = i;
+      pivot = compañeritos[i];
+      while((lowest > 0) && (compareString(compañeritos[lowest - 1].getName(), pivot.getName())){
+	compañeritos[lowest] = compañeritos[lowest - 1];
+	lowest--;
+      }
+      compañeritos[lowest] = pivot;
+    }
+  }
+  public static void insertionAlgorithmLastNameP(Student[] compañeritos){
+    int lowest;
+    Student pivot;
+    for(int i = 0; i < compañeritos.length; i++){
+      lowest = i;
+      pivot = compañeritos[i];
+      while((lowest > 0) && (compareString(compañeritos[lowest - 1].getLastNameP(), pivot.getLastNameP())){
+	compañeritos[lowest] = compañeritos[lowest - 1];
+	lowest--;
+      }
+      compañeritos[lowest] = pivot;
+    }
+  }
+  public static void insertionAlgorithmLastNameM(Student[] compañeritos){
+    int lowest;
+    Student pivot;
+    for(int i = 0; i < compañeritos.length; i++){
+      lowest = i;
+      pivot = compañeritos[i];
+      while((lowest > 0) && (compareString(compañeritos[lowest - 1].getLastNameM(), pivot.getLastNameM())){
+	compañeritos[lowest] = compañeritos[lowest - 1];
+	lowest--;
+      }
+      compañeritos[lowest] = pivot;
+    }
+  }
+  private static boolean compareEmail(String word1, String word2){
+    int index1 = word1.indexOf('@');
+    int index2 = word2.indexOf('@');
+    int length = word1.substring(0, index1).length();
+    if(length > word2.substring(0, index2).length()){
+      length = word2.substring(0, index2).length();
     }
     int i = 0;
     while(i < length){
       if(word1.charAt(i) == word2.charAt(i)){
-        continue;
+        i++;
+      }else{
+	if(word1.charAt(i) > word2.charAt(i)){
+	  return true;
+	}
+	return false;
       }
-      if(word1.charAt(i) > word2.charAt(i)){
-        return true;
+    }
+    return false;
+  }
+  private static boolean compareString(String word1, String word2){
+    String lower1 = word1.toLowerCase();
+    String lower2 = word1.toLowerCase();
+    int length = lower1.length();
+    if(length > lower2.length()){
+      length = lower2.length();	
+    }
+    int i = 0;
+    while(i < length){
+      if(lower1.charAt(i) == lower2.charAt(i)){
+	i++;
+      }else{
+	if(lower1.charAt(i) > lower2.charAt(i)){
+	  return true;
+	}
+	return false;
       }
     }
     return false;
