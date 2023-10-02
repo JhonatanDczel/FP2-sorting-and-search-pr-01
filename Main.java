f485f736ea570f1780eafb3f6f534faa02ef4a67
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2023-10-01 23:10:53 -0500

    Se añadió el método QuickSort para ordenar el array y el método para imprimir la lista se objetos

--- Main.java
+++ Main.java
@@ -2,9 +2,15 @@ public class Main{
     public static void main(String[] args){
         Reader lector = new Reader();
         Reader.Student[] estudiantes = lector.getData();
-        for (int i = 0; i < estudiantes.length; i++){
-            Reader.Student x = estudiantes[i];
-            System.out.println(x);
+        impr(estudiantes);
+        QuickSort.Cui(estudiantes, 0, estudiantes.length - 1);
+        impr(estudiantes);
+            
+    }
+    public static void impr(Reader.Student[] s) {
+        System.out.println("-----------------");
+        for (int i = 0; i < s.length; i++){
+            System.out.println(s[i].getCui());
         }
     }
 }
