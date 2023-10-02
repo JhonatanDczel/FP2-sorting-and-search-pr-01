8c46c706e6cf8c809351cd75b84941b99b690efa
Jorge_Luis <jmamanihuars@unsa.edu.pe>
2023-10-02 01:47:15 -0500

    Se llamó al método insertionCui de la clase Insertion para ver su funcionalidad, ¡sí ordena!

--- Main.java
+++ Main.java
@@ -1,16 +1,26 @@
 public class Main{
     public static void main(String[] args){
         Reader lector = new Reader();
-        Reader.Student[] estudiantes = lector.getData();
-        impr(estudiantes);
-        QuickSort.Cui(estudiantes, 0, estudiantes.length - 1);
-        impr(estudiantes);
-            
+        Reader.Student[] students = lector.getData();
+        impr(students);
+	long first, end;
+	
+	first = System.nanoTime();
+        QuickSort.Cui(students, 0, students.length - 1);
+	end = System.nanoTime();
+        impr(students);
+	System.out.println("\nTiempo: " + (end - first));
+	
+	first = System.nanoTime();
+	Insertion.insertionCui(students);
+	end = System.nanoTime();
+	impr(students);
+	System.out.println("\nTiempo: " + (end - first));
     }
     public static void impr(Reader.Student[] s) {
         System.out.println("-----------------");
         for (int i = 0; i < s.length; i++){
-            System.out.println(s[i].getCui());
+            s[i].print();
         }
     }
 }
