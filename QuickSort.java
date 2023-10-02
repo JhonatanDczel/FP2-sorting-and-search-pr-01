afde7b901d3ac3c8414a3d6976fde97bcbe9729d
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2023-10-01 23:11:32 -0500

    Se implementó el QuickSort para cui

--- QuickSort.java
+++ QuickSort.java
@@ -1,51 +1,24 @@
 import java.io.*;
 public class QuickSort {
-    public static void main(String[] args) {
-        Student s = new Student();
-        Student[] array;
-        
-        try {
-            array = s.mkStulist("data.csv");
-            impt(array);
-            quicksort(array, 0, array.length - 1);
-            System.out.println("----------");
-            impt(array);
-        } catch (FileNotFoundException e){
-            System.err.println("El archivo no se encontró: " + e.getMessage());
-        } catch (IOException e) {
-            System.err.println("Error al leer el archivo: " + e.getMessage());
-        } 
-
-    }
-    public static void impt (Student [] array) {
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i].getCui());
+    public static void Cui (Reader.Student[] s, int left, int right) {
+        int piv = s[left].getCui();
+        int i = left;
+        int j = right;
+        int aux;
+        while (i < j) {
+            while (s[i].getCui() <= piv && i < j) i++;
+            while (s[j].getCui() > piv) j--;
+            if (i < j) {
+                aux = s[i].getCui();
+                s[i].setCui(s[j].getCui()); 
+                s[j].setCui(aux);
+            }
         }
+        s[left].setCui(s[j].getCui());
+        s[j].setCui(piv);
+        if (left < j - 1)  Cui(s, left, j -1);
+        if (j + 1< right) Cui(s, j + 1, right);
+        
     }
-    public static void quicksort(Student array[], int izq, int der) {
 
-  int pivote=array[izq].getCui(); // tomamos primer elemento como pivote
-  int i=izq;         // i realiza la búsqueda de izquierda a derecha
-  int j=der;         // j realiza la búsqueda de derecha a izquierda
-  int aux;
- 
-  while(i < j){                          // mientras no se crucen las búsquedas                                   
-     while(array[i].getCui() <= pivote && i < j) i++; // busca elemento mayor que pivote
-     while(array[j].getCui() > pivote) j--;           // busca elemento menor que pivote
-     if (i < j) {                        // si no se han cruzado                      
-         aux= array[i].getCui();                      // los intercambia
-         array[i].setCui(array[j].getCui());
-         array[j].setCui(aux);
-     }
-   }
-   
-   array[izq].setCui(array[j].getCui());      // se coloca el pivote en su lugar de forma que tendremos                                    
-   array[j].setCui(pivote);      // los menores a su izquierda y los mayores a su derecha
-   
-   if(izq < j-1)
-      quicksort(array,izq,j-1);          // ordenamos subarray izquierdo
-   if(j+1 < der)
-      quicksort(array,j+1,der);          // ordenamos subarray derecho
-   
-}
 }
