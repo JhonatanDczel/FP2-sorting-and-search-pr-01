cda57aa68100e67a2c9f5ac809c67d11b231c28d
Jorge_Luis <jmamanihuars@unsa.edu.pe>
2023-10-02 01:41:17 -0500

    Se cambio el argumento de todos los métodos implementados, de arreglo de Student a arreglo de Reader.Student para posteriormente realizar las pruebas. Por último en colaboracion con las pruebas se corrigieron algunos errores de sintaxis y se eliminó el método convertion porque si el atributo DateOfBirth se compara convirtiendolo a entero, no cumple para todos los casos.

--- Insertion.java
+++ Insertion.java
@@ -1,7 +1,7 @@
 public class Insertion{
-  public static void insertionAlgorithmCui(Student[] compañeritos){
+  public static void insertionCui(Reader.Student[] compañeritos){
     int lowest;
-    Student pivot;
+    Reader.Student pivot;
     for(int i = 0; i < compañeritos.length; i++){
       lowest = i;
       pivot = compañeritos[i];
@@ -12,9 +12,9 @@ public class Insertion{
       compañeritos[lowest] = pivot;
     }
   }
-  public static void insertionAlgorithmEmail(Student[] compañeritos){
+  public static void insertionEmail(Reader.Student[] compañeritos){
     int lowest;
-    Student pivot;
+    Reader.Student pivot;
     for(int i = 0; i < compañeritos.length; i++){
       lowest = i;
       pivot = compañeritos[i];
@@ -25,9 +25,9 @@ public class Insertion{
       compañeritos[lowest] = pivot;
     }
   }
-  public static void insertionAlgorithmName(Student[] compañeritos){
+  public static void insertionName(Reader.Student[] compañeritos){
     int lowest;
-    Student pivot;
+    Reader.Student pivot;
     for(int i = 0; i < compañeritos.length; i++){
       lowest = i;
       pivot = compañeritos[i];
@@ -38,22 +38,22 @@ public class Insertion{
       compañeritos[lowest] = pivot;
     }
   }
-  public static void insertionAlgorithmLastNameP(Student[] compañeritos){
+  public static void insertionLastNameF(Reader.Student[] compañeritos){
     int lowest;
-    Student pivot;
+    Reader.Student pivot;
     for(int i = 0; i < compañeritos.length; i++){
       lowest = i;
       pivot = compañeritos[i];
-      while((lowest > 0) && (compareString(compañeritos[lowest - 1].getLastNameP(), pivot.getLastNameP()))){
+      while((lowest > 0) && (compareString(compañeritos[lowest - 1].getLastNameF(), pivot.getLastNameF()))){
 	compañeritos[lowest] = compañeritos[lowest - 1];
 	lowest--;
       }
       compañeritos[lowest] = pivot;
     }
   }
-  public static void insertionAlgorithmLastNameM(Student[] compañeritos){
+  public static void insertionLastNameM(Reader.Student[] compañeritos){
     int lowest;
-    Student pivot;
+    Reader.Student pivot;
     for(int i = 0; i < compañeritos.length; i++){
       lowest = i;
       pivot = compañeritos[i];
@@ -65,13 +65,13 @@ public class Insertion{
     }
   }
 
-  public static void insertionAlgorithmDateOfBirth(Student[] compañeritos){
+  public static void insertionDateOfBirth(Reader.Student[] compañeritos){
     int lowest;
-    Student pivot;
+    Reader.Student pivot;
     for(int i = 0; i < compañeritos.length; i++){
       lowest = i;
       pivot = compañeritos[i];
-      while((lowest > 0) && (convertion(compañeritos[lowest - 1].getDateOfBirth()) > convertion(pivot.getDateOfBirth()))){
+      while((lowest > 0) && (compareString(compañeritos[lowest - 1].getDateOfBirth(), pivot.getDateOfBirth()))){
         compañeritos[lowest] = compañeritos[lowest - 1];
         lowest--;
       }
@@ -79,13 +79,13 @@ public class Insertion{
     }
   }
 
-  public static void insertionAlgorithmGender(Student[] compañeritos){
+  public static void insertionGender(Reader.Student[] compañeritos){
     int lowest;
-    Student pivot;
+    Reader.Student pivot;
     for(int i = 0; i < compañeritos.length; i++){
       lowest = i;
       pivot = compañeritos[i];
-      while((lowest > 0) && (comparString(compañeritos[lowest - 1].getDateOfBirth(), pivot.getDateOfBirth()))){
+      while((lowest > 0) && (compañeritos[lowest - 1].getGender().charAt(i) > pivot.getGender().charAt(i))){
         compañeritos[lowest] = compañeritos[lowest - 1];
         lowest--;
       }
@@ -114,18 +114,18 @@ public class Insertion{
     return false;
   }
   private static boolean compareString(String word1, String word2){
-    String lower1 = word1.toLowerCase();
-    String lower2 = word1.toLowerCase();
-    int length = lower1.length();
-    if(length > lower2.length()){
-      length = lower2.length();	
+    //Suponemos que los datos no tienen mayusculas donde no deben. Además este método nos
+    //sirve para ordenar según nacimiento, pues si convertimos a número, no cumple todos los casos
+    int length = word1.length();
+    if(length > word2.length()){
+      length = word2.length();	
     }
     int i = 0;
     while(i < length){
-      if(lower1.charAt(i) == lower2.charAt(i)){
+      if(word1.charAt(i) == word2.charAt(i)){
 	i++;
       }else{
-	if(lower1.charAt(i) > lower2.charAt(i)){
+	if(word1.charAt(i) > word2.charAt(i)){
 	  return true;
 	}
 	return false;
@@ -133,10 +133,4 @@ public class Insertion{
     }
     return false;
   }
-
-  public static int convertion(String one){
-    int a = parseInteger(one.substring(0, 4) + one.substring(4,6) + one.substring(6,8));
-    return a;
-  }
-
 }
