14d481e94371a01d585450b0e4e6d5790db6c539
dmolloc1 <dmolloc@unsa.edu.pe>
2023-10-02 10:53:06 -0500

    Se agrego el metodo insertionStatus() y algunas correcciones

--- Insertion.java
+++ Insertion.java
@@ -1,98 +1,114 @@
 public class Insertion{
-  public static void insertionCui(Reader.Student[] compañeritos){
+  public static void insertionCui(Reader.Student[] classmates){
     int lowest;
     Reader.Student pivot;
-    for(int i = 0; i < compañeritos.length; i++){
+    for(int i = 0; i < classmates.length; i++){
       lowest = i;
-      pivot = compañeritos[i];
-      while((lowest > 0) && (compañeritos[lowest - 1].getCui() > pivot.getCui())){
-	compañeritos[lowest] = compañeritos[lowest - 1];
+      pivot = classmates[i];
+      while((lowest > 0) && (classmates[lowest - 1].getCui() > pivot.getCui())){
+	classmates[lowest] = classmates[lowest - 1];
 	lowest--;
       }
-      compañeritos[lowest] = pivot;
+      classmates[lowest] = pivot;
     }
   }
-  public static void insertionEmail(Reader.Student[] compañeritos){
+  public static void insertionEmail(Reader.Student[] classmates){
     int lowest;
     Reader.Student pivot;
-    for(int i = 0; i < compañeritos.length; i++){
+    for(int i = 0; i < classmates.length; i++){
       lowest = i;
-      pivot = compañeritos[i];
-      while((lowest > 0) && (compareEmail(compañeritos[lowest - 1].getEmail(), pivot.getEmail()))){
-        compañeritos[lowest] = compañeritos[lowest - 1];
+      pivot = classmates[i];
+      while((lowest > 0) && (compareEmail(classmates[lowest - 1].getEmail(), pivot.getEmail()))){
+        classmates[lowest] = classmates[lowest - 1];
         lowest--;
       }
-      compañeritos[lowest] = pivot;
+      classmates[lowest] = pivot;
     }
   }
-  public static void insertionName(Reader.Student[] compañeritos){
+  public static void insertionName(Reader.Student[] classmates){
     int lowest;
     Reader.Student pivot;
-    for(int i = 0; i < compañeritos.length; i++){
+    for(int i = 0; i < classmates.length; i++){
       lowest = i;
-      pivot = compañeritos[i];
-      while((lowest > 0) && (compareString(compañeritos[lowest - 1].getName(), pivot.getName()))){
-	compañeritos[lowest] = compañeritos[lowest - 1];
+      pivot = classmates[i];
+      while((lowest > 0) && (compareString(classmates[lowest - 1].getName(), pivot.getName()))){
+	classmates[lowest] = classmates[lowest - 1];
 	lowest--;
       }
-      compañeritos[lowest] = pivot;
+      classmates[lowest] = pivot;
     }
   }
-  public static void insertionLastNameF(Reader.Student[] compañeritos){
+  public static void insertionLastNameF(Reader.Student[] classmates){
     int lowest;
     Reader.Student pivot;
-    for(int i = 0; i < compañeritos.length; i++){
+    for(int i = 0; i < classmates.length; i++){
       lowest = i;
-      pivot = compañeritos[i];
-      while((lowest > 0) && (compareString(compañeritos[lowest - 1].getLastNameF(), pivot.getLastNameF()))){
-	compañeritos[lowest] = compañeritos[lowest - 1];
+      pivot = classmates[i];
+      while((lowest > 0) && (compareString(classmates[lowest - 1].getLastNameF(), pivot.getLastNameF()))){
+	classmates[lowest] = classmates[lowest - 1];
 	lowest--;
       }
-      compañeritos[lowest] = pivot;
+      classmates[lowest] = pivot;
     }
   }
-  public static void insertionLastNameM(Reader.Student[] compañeritos){
+  public static void insertionLastNameM(Reader.Student[] classmates){
     int lowest;
     Reader.Student pivot;
-    for(int i = 0; i < compañeritos.length; i++){
+    for(int i = 0; i < classmates.length; i++){
       lowest = i;
-      pivot = compañeritos[i];
-      while((lowest > 0) && (compareString(compañeritos[lowest - 1].getLastNameM(), pivot.getLastNameM()))){
-	compañeritos[lowest] = compañeritos[lowest - 1];
+      pivot = classmates[i];
+      while((lowest > 0) && (compareString(classmates[lowest - 1].getLastNameM(), pivot.getLastNameM()))){
+	classmates[lowest] = classmates[lowest - 1];
 	lowest--;
       }
-      compañeritos[lowest] = pivot;
+      classmates[lowest] = pivot;
     }
   }
 
-  public static void insertionDateOfBirth(Reader.Student[] compañeritos){
+  public static void insertionDateOfBirth(Reader.Student[] classmates){
     int lowest;
     Reader.Student pivot;
-    for(int i = 0; i < compañeritos.length; i++){
+    for(int i = 0; i < classmates.length; i++){
       lowest = i;
-      pivot = compañeritos[i];
-      while((lowest > 0) && (compareString(compañeritos[lowest - 1].getDateOfBirth(), pivot.getDateOfBirth()))){
-        compañeritos[lowest] = compañeritos[lowest - 1];
+      pivot = classmates[i];
+      while((lowest > 0) && (compareString(classmates[lowest - 1].getDateOfBirth(), pivot.getDateOfBirth()))){
+        classmates[lowest] = classmates[lowest - 1];
         lowest--;
       }
-      compañeritos[lowest] = pivot;
+      classmates[lowest] = pivot;
     }
   }
 
-  public static void insertionGender(Reader.Student[] compañeritos){
+  public static void insertionGender(Reader.Student[] classmates){
     int lowest;
     Reader.Student pivot;
-    for(int i = 0; i < compañeritos.length; i++){
+    insertionLastNameF(classmates); //Al solo haber dos opciones se hace uso de este metodo para que además de separar por generos se mantenga un orden en cada conjunto
+    for(int i = 0; i < classmates.length; i++){
       lowest = i;
-      pivot = compañeritos[i];
-      while((lowest > 0) && (compañeritos[lowest - 1].getGender().charAt(i) > pivot.getGender().charAt(i))){
-        compañeritos[lowest] = compañeritos[lowest - 1];
+      pivot = classmates[i];
+      while((lowest > 0) && (classmates[lowest - 1].getGender().charAt(0) > pivot.getGender().charAt(0))){
+        classmates[lowest] = classmates[lowest - 1];
         lowest--;
       }
-      compañeritos[lowest] = pivot;
+      classmates[lowest] = pivot;
     }
   }
 
+  public static void insertionStatus(Reader.Student[] classmates){
+    int lowest;
+    Reader.Student pivot;
+	insertionLastNameF(classmates);
+    for(int i = 0; i < classmates.length; i++){
+      lowest = i;
+      pivot = classmates[i];
+      while((lowest > 0) && (classmates[lowest - 1].getStatus().charAt(0) > pivot.getStatus().charAt(0))){
+        classmates[lowest] = classmates[lowest - 1];
+        lowest--;
+      }
+      classmates[lowest] = pivot;
+    }
+  }
+	
   private static boolean compareEmail(String word1, String word2){
     int index1 = word1.indexOf('@');
     int index2 = word2.indexOf('@');
--- Main.java
+++ Main.java
@@ -2,7 +2,6 @@ public class Main{
     public static void main(String[] args){
         Reader lector = new Reader();
         Reader.Student[] students = lector.getData();
-        impr(students);
 	long first, end;
 	
 	first = System.nanoTime();
@@ -12,7 +11,7 @@ public class Main{
 	System.out.println("\nTiempo: " + (end - first));
 	
 	first = System.nanoTime();
-	Insertion.insertionCui(students);
+	Insertion.insertionStatus(students);
 	end = System.nanoTime();
 	impr(students);
 	System.out.println("\nTiempo: " + (end - first));
