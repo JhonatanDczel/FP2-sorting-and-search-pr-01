55e501872e486ef65d414a41aeb9f673c6b8318e
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2023-10-03 07:20:51 -0500

    Se creó el método cui para la búsqueda binaria

--- algorithms/IterativeBinarySearch.java
+++ algorithms/IterativeBinarySearch.java
@@ -1,3 +1,19 @@
+package algorithms;
+
+import reader.Reader;
+import java.io.*;
 public class IterativeBinarySearch {
+  public static int cui (Reader.Student [] s, int x) {
+    int l, r;
+    l = 0;
+    r = s.length - 1;
+    while (l <= r) {
+      int m = l + ( r - l ) / 2;
+      if (s[m].getCui() == x) return m;
+      if (s[m].getCui() < x) l = m + 1;
+      else r = m - 1;
+    }
+    return -1;
+  }
     
 }
