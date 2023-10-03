b90e4fbe4c4373de351edf016e255e9534088251
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2023-10-03 07:42:02 -0500

    Implementación par busqueda según email

--- algorithms/IterativeBinarySearch.java
+++ algorithms/IterativeBinarySearch.java
@@ -15,5 +15,23 @@ public class IterativeBinarySearch {
     }
     return -1;
   }
+  public static int email (Reader.Student [] s, String x) {
+    int l = 0, r = s.length - 1;
+    while (l <= r) {
+      int m = l + (r - l) / 2; 
+      if (s[m].getEmail().equals(x)) return m;
+      if (smallerThan(s[m].getEmail(), x)) l = m + 1;
+      else r = m - 1;
+    }
+    return -1;
+  }
+  
+  public static boolean smallerThan(String w1, String w2) {
+      w1.toUpperCase();
+      w2.toUpperCase();
+      if (w1.compareTo(w2) <= 0) return true;
+      return false;
+
+    }
     
 }
