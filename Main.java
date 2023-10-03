c5d0d2b903a15abb7689bb709cb1da2f1b39e414
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2023-10-03 15:09:34 -0500

    Probando algunos métodos

--- Main.java
+++ Main.java
@@ -4,11 +4,11 @@ public class Main{
     public static void main(String[] args){
         Reader lector = new Reader();
         Reader.Student[] students = lector.getData();
-        QuickSort.email(students, 0, students.length - 1);
+        Insertion.insertionEmail(students);
         impr(students);
-//        int idx = RecursiveBinarySearch.email(students,"jgarciafernandez@unsa.edu.pe", 0, students.length - 1);
-//        System.out.println(idx);
-//        System.out.println(students[idx]);
+        int idx = IterativeBinarySearch.email(students,"jgarciafernandez@unsa.edu.pe");
+        System.out.println(idx);
+        System.out.println(students[idx]);
 /*
 	    long first, end;
 	
