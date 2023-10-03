4945626b04f76013df158f9c3bef9552fc293878
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2023-10-03 07:42:19 -0500

    Prueba de busqueda por email

--- Main.java
+++ Main.java
@@ -4,8 +4,8 @@ public class Main{
     public static void main(String[] args){
         Reader lector = new Reader();
         Reader.Student[] students = lector.getData();
-        QuickSort.cui(students, 0, students.length - 1);
-        int idx = IterativeBinarySearch.cui(students, 24345678);
+        QuickSort.email(students, 0, students.length - 1);
+        int idx = IterativeBinarySearch.email(students,"mmartinezlopez@unsa.edu.pe");
         System.out.println(idx);
         System.out.println(students[idx]);
 /*
