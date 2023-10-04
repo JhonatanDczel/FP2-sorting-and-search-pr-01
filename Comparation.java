957838d51c4331610dec8c0d75a7bc3d1b3d2732
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2023-10-04 10:35:24 -0500

    Se avanzó con el menú de Quicksort

--- Comparation.java
+++ Comparation.java
@@ -90,7 +90,43 @@ public class Comparation{
           System.out.println("6. F. nacimiento");
           System.out.println("7. Genero");
           System.out.println("8. Status");
-
+          opcion = sc.nextInt();
+          switch (opcion) {
+            case 1:
+              QuickSort.cui(s, 0, s.length - 1);
+              printStudents(s);
+              break;
+            case 2:
+              QuickSort.email(s, 0, s.length - 1);
+              printStudents(s);
+              break;
+            case 3:
+              QuickSort.name(s, 0, s.length - 1);
+              printStudents(s);
+              break;
+            case 4:
+              QuickSort.lastNameF(s, 0, s.length - 1);
+              printStudents(s);
+              break;
+            case 5:
+              QuickSort.lastNameM(s, 0, s.length - 1);
+              printStudents(s);
+              break;
+            case 6:
+              QuickSort.dateOfBirth(s, 0, s.length - 1);
+              printStudents(s);
+              break;
+            case 7:
+              QuickSort.gender(s, 0, s.length - 1);
+              printStudents(s);
+              break;
+            case 8:
+              QuickSort.status(s, 0, s.length - 1);
+              printStudents(s);
+              break;
+            default:
+              System.out.println("Opción no válida. Por favor, seleccione una opción válida.");
+          }
           System.exit(0);
           break;
         case 3:
