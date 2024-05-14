6f3fae60800f176ce63f179698a54a8258eccfe9
JhonatanDczel <jariasq@unsa.edu.pe>
2024-05-14 00:28:53 -0500

    Checkpoint

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main{
     public static void main(String[] args){
         Reader lector = new Reader();
         Reader.Student[] students = lector.getData();
-        Insertion.insertionEmail(students);
+        InsertionSort.email(students);
         impr(students);
         int idx = IterativeBinarySearch.email(students,"jgarciafernandez@unsa.edu.pe");
         System.out.println(idx);
